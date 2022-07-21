1ebf7f2b133458a1d33fbb169ca72ccb93d85774
Darya Hudzovska <102170750+bestboost@users.noreply.github.com>
2022-07-21 17:39:52 +0300

    bug

--- jsTasks/Mod6Les12.js
+++ jsTasks/Mod6Les12.js
@@ -7,6 +7,8 @@
 //         e.currentTarget.value;
 // });
 
+
+
 // Calculator
 const operations = ['+', '-', '*', '/', '^'];
 
@@ -49,3 +51,66 @@ firstInput.onchange = render;
 // (the same - firstInput.addEventListener("change", render); )
 select.onchange = render;
 secondInput.onchange = render;
+
+const textToCalculate = `555 + 7 * 6 - 3 % 6 - 3 * 9 + 2 % -6 * -6 * 9`;
+
+const complexOperation = ['*', '%'];
+
+function calculateSubtractBlock(subtractionBlock) {
+    const charArr = subtractionBlock.split('');
+    const resultBlock = [];
+    let currentBlock = '';
+
+    for ( let i = 0; i < charArr.length; i ++ ) {
+        const char = charArr[i];
+        if (i > 0 && char === '-' && !complexOperation.includes(charArr[i - 1])) {
+            resultBlock.push(currentBlock)
+            currentBlock = ''
+        } else {
+            currentBlock += char;
+        }
+
+        if (i === charArr.length - 1) {
+            resultBlock.push(currentBlock);
+        }
+    }
+    return resultBlock;
+};
+
+function calculateDivideBlock(divideBlock) {
+    return divideBlock.split('%')
+}
+
+function calculateMultiplyBlock(multiplyBlock) {
+    return multiplyBlock.split('*')
+        .map(n => Number(n))
+        .reduce((a, b) => a * b, 1);
+}
+
+function calculateAll(textToCalculate) {
+     const parsedText = textToCalculate.replaceAll(' ', '');
+
+    const addParts = parsedText.split('+')
+    addParts.map(addPart => {
+        const subtractParts = calculateSubtractBlock(addPart)
+        console.log(subtractParts)
+
+        const resultOfSubtractParts = subtractParts.map(subPart => {
+
+            const resultOfDivideBlocks = calculateDivideBlock(subPart)
+                .map(calculateMultiplyBlock)
+                .reduce((a, b) => a % b)
+            console.log(resultOfDivideBlocks)
+            return resultOfDivideBlocks;
+        }).reduсe((a, b) => a - b);
+        console.log(resultOfSubtractParts)
+    })
+}
+
+// console.log(calculateSubtractBlock('7*6-3%6-3*9'));
+// console.log(calculateSubtractBlock('2%-6*-6*9'));
+
+// console.log(calculateDivideBlock('2%-6*-6*9'));
+// console.log(calculateMultiplyBlock('-6*-6*9'));
+
+calculateAll(textToCalculate);
